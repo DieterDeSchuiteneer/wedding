ee46766d4945ea6b4cc3a4adc1b7acf5d4388512
Dieter De Schuiteneer <dieter.de.schuiteneer@hotmail.com>
2026-07-26 23:04:29 +0200

    simplify hero

--- app/_components/HeroSectionHome.tsx
+++ app/_components/HeroSectionHome.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { missFajardose } from "../layout";
 
 export default function HeroSectionHome() {
   return (
@@ -8,24 +7,10 @@ export default function HeroSectionHome() {
       <div className="bg-white/20 backdrop-blur-sm w-full h-full z-10 " />
       <div className="overflow-hidden text-center absolute top-0 w-full my-16 ">
         <h1
-          className={`text-8xl starting:translate-y-full  delay-400 transition-all translate-0 ease-out duration-200`}
+          className={`text-8xl starting:opacity-0 opacity-100 starting:translate-y-full  delay-400 transition-opacity translate-0 duration-200`}
         >
           Wij trouwen!
         </h1>
-      </div>
-      <div className={` pl-2 text-6xl absolute top-1/2 overflow-hidden`}>
-        <p className="w-full  starting:translate-x-full  delay-600 transition-all translate-0 ease-out duration-200 ">
-          Wie
-        </p>
-        <p className="w-full starting:translate-x-full  delay-700 transition-all translate-0 ease-out duration-200 ">
-          Wat
-        </p>
-        <p className="w-full  starting:translate-x-full  delay-800 transition-all translate-0 ease-out duration-200 ">
-          Waar
-        </p>
-        <p className="w-full  starting:translate-x-full  delay-900 transition-all translate-0 ease-out duration-200 ">
-          Wanneer
-        </p>
       </div>
     </div>
   );
