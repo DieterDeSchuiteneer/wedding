f8640c9776e404aa5d3b534230d00fcea83b9a0f
Dieter De Schuiteneer <dieter.de.schuiteneer@hotmail.com>
2026-07-28 23:11:57 +0200

    playing around

--- app/_components/timeline/TimelineItem.tsx
+++ app/_components/timeline/TimelineItem.tsx
@@ -19,20 +19,25 @@ export default function TimeLineItem({
   location,
 }: Props) {
   return (
-    <FlipCard offset={0.75} className={`flex gap-4 ${coiny.className}`}>
+    <FlipCard
+      offset={0.75}
+      className={`flex items-center gap-4 ${coiny.className}`}
+    >
       <>
         <div className=" rounded-full size-20 flex items-center justify-center ">
           {icon}
         </div>
-        <div>
-          <p className={`text-gray-700 ${limeLight.className} `}>
+        <div className="">
+          <p className={`text-mauve-800  ${limeLight.className} `}>
             {start} - {end}
           </p>
-          <p className=" text-xl">{description}</p>
+          <p className="leading-tight text-gray-700">{description}</p>
           {location && (
-            <div className="flex gap-1 text-xl">
-              <IconDirections size={24} stroke={1} />
-              {location}
+            <div className="flex gap-1 text-xl mt-1">
+              <IconDirections size={16} stroke={1} />
+              <a href="" className="text-sm underline">
+                {location}
+              </a>
             </div>
           )}
         </div>
