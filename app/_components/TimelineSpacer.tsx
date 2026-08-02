1a9eed71c981d9134b3d5755e73ef25b47dd67a3
Dieter De Schuiteneer <dieter.de.schuiteneer@hotmail.com>
2026-08-02 21:18:21 +0200

    sapcer fix

--- app/_components/TimelineSpacer.tsx
+++ app/_components/TimelineSpacer.tsx
@@ -3,17 +3,13 @@ import { cn } from "@sglara/cn";
 import { useInView } from "react-intersection-observer";
 
 export default function () {
-  const { ref, inView } = useInView({
-    threshold: 0.5,
-  });
-
   return (
     <div
       className={cn(
-        "ml-6 my-1 h-11 w-0 transition-all duration-350 border-transparent opacity-40",
-        inView ? "border-gray-400 border" : ""
+        "ml-10 my-1 h-11 w-0 transition-all duration-350 border border-gray-400  starting:opacity-0"
       )}
-      ref={ref}
-    />
+    >
+      <div className="h-full w-full bg-blue-600 origin-top animate-[scaleY_linear_both] [animation-timeline:scroll()]"></div>
+    </div>
   );
 }
