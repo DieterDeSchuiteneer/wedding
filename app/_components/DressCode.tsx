9b103a0b269b7470cd6e7fbd1f49efb1cde46b7b
Dieter De Schuiteneer <dieter.de.schuiteneer@hotmail.com>
2026-08-11 08:19:54 +0200

    better image quality

--- app/_components/DressCode.tsx
+++ app/_components/DressCode.tsx
@@ -13,7 +13,7 @@ export default function Dresscode() {
             objectFit="cover"
             objectPosition="20% "
           />
-          <p className="absolute center top-6 text-white font-retro bg-amber-400">
+          <p className="absolute center top-6 text-white font-retro text-shadow-md text-shadow-zinc-950 w-3/4 left-1/2 -translate-x-1/2">
             Strak in het pak of een prachtig kleed. De kleuren laten wij aan
             jullie over!
           </p>
